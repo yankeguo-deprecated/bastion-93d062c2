a69b8acbdad922f681703cdd7833b31edf7c9bfd
Yanke Guo <ryan@islandzero.net>
2017-09-12 21:29:41 +0800

    complete users/:id

--- routes/user/show.go
+++ routes/user/show.go
@@ -1,19 +1,40 @@
 package user
 
 import (
+	"fmt"
+	"github.com/pagoda-tech/bastion/models"
 	"github.com/pagoda-tech/bastion/routes/middlewares"
 	"github.com/pagoda-tech/macaron"
 )
 
 // Show 显示一个用户
-func Show(ctx *macaron.Context, r *middlewares.Render, a middlewares.Auth) {
+func Show(ctx *macaron.Context, r *middlewares.Render, a middlewares.Auth, db *models.DB) {
+	// validate signed in
 	if !a.SignedIn() {
 		r.Fail(a.Code, a.Message)
 		return
 	}
+
+	// extract current user if 'current'
 	id := ctx.Params(":id")
-	if id == "current" {
+	if id == "current" || id == fmt.Sprint(a.CurrentUser.ID) {
 		r.Success("user", a.CurrentUser)
 		return
+	}
+
+	// check IsAdmin
+	if !a.CurrentUser.IsAdmin {
+		r.Fail("not_found", "没有找到该用户")
+		return
+	}
+
+	// find
+	u := &models.User{}
+	db.First(u, id)
+
+	if !db.NewRecord(u) {
+		r.Success("user", u)
+	} else {
+		r.Fail("not_found", "没有找到该用户")
 	}
 }
