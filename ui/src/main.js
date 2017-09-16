9d413732ba7643cebbd9155ff8348698ce7e9627
Yanke Guo <ryan@islandzero.net>
2017-09-17 02:16:24 +0800

    modify table

--- ui/src/main.js
+++ ui/src/main.js
@@ -5,6 +5,7 @@ import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/css/bootstrap-modify.css'
 
 Vue.use(BootstrapVue)
 Vue.use(VueResource)
