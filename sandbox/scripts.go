8e0ecb44188cd324b9fe5c7925ecbfa47cbd5c26
Yanke Guo <ryan@islandzero.net>
2017-10-30 02:11:16 +0800

    add script for SSH

--- sandbox/scripts.go
+++ sandbox/scripts.go
@@ -24,6 +24,27 @@ chmod 600 id_rsa
 echo "id_rsa 和 id_rsa.pub 受 Bunker 管理，请勿修改" > README
 `
 
+const tplSSHConfig = `#!/bin/bash
+# remove .ssh/config
+rm -f /root/.ssh/config
+
+# create new .ssh/config
+{{range .Entries}}
+echo "Host {{.Name}}" >> /root/.ssh/config
+echo "  HostName {{.Host}}" >> /root/.ssh/config
+echo "  Port {{.Port}}" >> /root/.ssh/config
+echo "  User {{.User}}" >> /root/.ssh/config
+{{end}}
+`
+
+// SSHEntry a entry in ssh_config
+type SSHEntry struct {
+	Name string
+	Host string
+	Port int
+	User string
+}
+
 func createScript(name string, tmpl string, data com.Map) string {
 	t, err := template.New(name).Parse(tmpl)
 	if err != nil {
@@ -39,5 +60,13 @@ func scriptSeedSSH(publicKey string, privateKey string) string {
 		"seed-ssh",
 		tplSeedSSH,
 		com.NewMap("PublicKey", publicKey, "PrivateKey", privateKey),
+	)
+}
+
+func scriptSeedSSHConfig(entries []SSHEntry) string {
+	return createScript(
+		"seed-ssh-config",
+		tplSSHConfig,
+		com.NewMap("Entries", entries),
 	)
 }
